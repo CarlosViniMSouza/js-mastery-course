531e30e6a1757535cc749908882d8981750ba2a5
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-02-18 20:49:46 -0300

    Stop Moment: Operators, Booleans, Selection in JS

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add -A 
-git commit -m "Stop Moment: Module 2 Introduction"
+git commit -m "Stop Moment: Operators, Booleans, Selection in JS"
 git push -u origin main
 git status
 
-# time: 1:05:51
+# time: 1:27:00
